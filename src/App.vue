b47561041b021e68bfb31e7ccc6375fc1ff62a64
francamateus93 <francamateus@gmail.com>
2024-07-26 13:28:13 +0200

    import tattoo-page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script>
-
-</script>
 
 <template>
  
+ <TattooPage></TattooPage>
   
 </template>
+
+<script>
+import TattooPage from './components/Tattoo-page.vue';
+
+  export default {
+    name: "App",
+    components: {TattooPage},
+  data() {
+    return {
+    }
+  }
+}
+</script>
 
 <style>
 
--- src/components/Tattoo-page.vue
+++ src/components/Tattoo-page.vue
@@ -1,10 +1,17 @@
 <template>
   <label for=""></label>
   <input type="text">
-  <button></button>
-  
+  <button>Submit</button>
+
 </template>
 
 <script>
+export default {
+  name: "Tattoo-page",
+  data() {
+    return {
 
+    }
+  }
+}
 </script>
