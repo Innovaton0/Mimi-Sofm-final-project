05bcb8c9ada3eb7a1451154f4e46419554757f35
Marian Simó Díaz <mad@MADMacOS.local>
2024-07-28 14:29:04 +0200

    Style created using CSS instead of Tailwind - but could not preview it

--- src/components/Landing-page.vue
+++ src/components/Landing-page.vue
@@ -1,8 +1,19 @@
 <template></template>
     <div class="container">
-        <div class="left-section">
-            <img src="components/images/landing-page/lef-image.jpg"  alt="woman with tattooed arm"
+        <div class="left-image">
+            <img 
+            src="components/images/landing-page/left-image.jpg"
+            alt="woman with tattooed arm"
             />
+        </div>
+        <div class="center section">
+            <img 
+            src="components/images/lading-page-ilustracion.png"
+            alt="center illustration"
+            class= "logo"
+            />
+            <h1>MEJORES TATUAJES & <br />TATUADORES DE BARCELONA</h1>
+            <a href="link?" target="_blank" class="home-link">Home</a>
         </div>
     </div>
 
