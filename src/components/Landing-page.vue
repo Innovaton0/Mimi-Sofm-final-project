54285055c00a42f236f73b3a1c3dce7f7536e662
Marian Simó Díaz <mad@MADMacOS.local>
2024-07-28 13:26:54 +0200

    template-HTML created

--- src/components/Landing-page.vue
+++ src/components/Landing-page.vue
@@ -1,4 +1,10 @@
 <template></template>
+    <div class="container">
+        <div class="left-section">
+            <img src="components/images/landing-page/lef-image.jpg"  alt="woman with tattooed arm"
+            />
+        </div>
+    </div>
 
 <script></script>
 
