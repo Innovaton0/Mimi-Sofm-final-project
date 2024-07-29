f4c927078dfa98b85acff40b775a99b107d17798
NachadeBosquetro <ignaciaossandon@gmail.com>
2024-07-29 13:21:38 +0200

    Tattoo 2 created

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,11 @@
 import TattooPage from "./components/TattooPage.vue";
 import LandingPage from "./components/LandingPage.vue";
 import Home from "./components/Home.vue";
+import TattooPage2 from "./components/TattooPage2.vue";
 
 export default {
   name: "App",
-  components: { Home, TattooPage, LandingPage },
+  components: { Home, TattooPage, LandingPage, TattooPage2 },
   data() {
     return {};
   },
