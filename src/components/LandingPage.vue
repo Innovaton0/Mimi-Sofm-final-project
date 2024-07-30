4c4ebbc63eeedc5cb66d6d7ef5add3234a3547fb
Marian Simó Díaz <mad@MADMacOS.local>
2024-07-30 13:55:53 +0200

    design landingpage day 1 - Inno and Marianto

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="container">
-    <div class="left-image">
+  <div class="grid grid-cols-3 grid-rows-1 h-screen">
+    <!-- Left side -->
+    <div class="col-start-1 col-end-2 row-start-1">
       <img
+        class="h-screen"
         src="../images/landing-page/left-image.jpg"
         alt="woman with tattooed arm"
       />
     </div>
-    <div class="center-section">
-      <img
-        src="../images/landing-page/ilustracion.png"
-        alt="center illustration"
-        class="logo"
-      />
-      <h1>MEJORES TATUAJES & <br />TATUADORES DE BARCELONA</h1>
+
+    <!-- Center section -->
+    <div class="bg-black h-screen col-start-2 col-end-3 row-start-1">
+      <h1 class="text-white">MEJORES TATUAJES & <br />TATUADORES DE BARCELONA</h1>
       <a href="link?" target="_blank" class="home-link">Home</a>
     </div>
-    <div class="right-section">
+    <div
+      class="flex justify-center items-center col-start-1 col-end-4 row-start-1"
+    >
       <img
+        class="w-[45rem] h-120 opacity-25"
+        src="../images/landing-page/ilustracion.png"
+        alt="center illustration"
+      />
+    </div>
+
+    <!-- Right section -->
+    <div class="col-start-3 col-end-4 row-start-1">
+      <img
+        class="h-screen"
         src="../images/landing-page/right-image.jpg"
         alt="woman with several tattoos looking at camera"
       />
