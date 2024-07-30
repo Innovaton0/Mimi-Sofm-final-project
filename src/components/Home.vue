a684cb20411c2da54322940f375d4481596cd9be
NachadeBosquetro <ignaciaossandon@gmail.com>
2024-07-30 13:42:56 +0200

    home almost finish. footer finish

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,38 +14,85 @@
 </section>
 
   <!-- FOTOS Y  FONDO BEIGE -->
-  <div class="h-[200px] bg-[#F9EEDF]">
-    
-  <span> foto </span>
-
+  <section class="bg-[#F9EEDF] h-[200px]">
+  <div class="border border-black p-5 flex justify-left items-center space-x-4">
+    <div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Imagen Redonda"
+        class="w-20 h-20 rounded-full object-cover"
+      />
+    </div>
+    <div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Imagen Redonda"
+        class="w-20 h-20 rounded-full object-cover"
+      />
+    </div>
+    <div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Imagen Redonda"
+        class="w-20 h-20 rounded-full object-cover"
+      />
+    </div>
   </div>
+</section>
+
+<section class="bg-[#F9EEDF] h-[200px]">
+  <div class="border border-black p-5 flex col-span-4 justify-end space-x-4">
+    <div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Imagen Redonda"
+        class="w-20 h-20 rounded-full object-cover"
+      />
+    </div>
+    <div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Imagen Redonda"
+        class="w-20 h-20 rounded-full object-cover"
+      />
+    </div>
+    <div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Imagen Redonda"
+        class="w-20 h-20 rounded-full object-cover"
+      />
+    </div>
+  </div>
+</section>
 
 </header>
 
 
 
 <footer class="bg-[#8A312E] text-white p-20">
-  <div class="flex gap-10 border-white border">
+  <div class="flex gap-10 justify-between">
     <img src="/src/assets/logo_larama.png" alt="logo" class="h-36">
-    <ul class="flex flex-col gap-1">
+    <ul class="flex flex-col justify-center gap-1">
       <li><span>(+34) 600 00 00 00</span></li>
-      <li><span>345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345</span></li>
-      <ul class="flex gap-0 border border-red-400">
+      <li><span>345 Faulconer Drive, Suite 4 • </span></li><li><span>Charlottesville, CA, 12345</span></li>
+      <ul class="flex gap-5">
+        <li><i class="fa-brands fa-facebook"></i></li>
+        <li><i class="fa-brands fa-square-instagram"></i></li>
         <li><i class="fa-brands fa-twitter"></i></li>
-        <li><svg></svg>instagram</li>
-        <li><svg></svg>twitter</li>
+        <li><i class="fa-brands fa-youtube"></i></li>
       </ul>
-      </ul>
+    </ul>
   </div>
 
-    <div class="border-yellow-300 text-white">
-     <ul class="flex gap-2">
-      <li><a href="#" class="uppercasse">about us</a></li>
-      <li><a href="#" class="uppercasse">contact us</a></li>
-      <li><a href="#" class="uppercasse">help</a></li>
-      <li><a href="#" class="uppercasse">privacy policy</a></li>
-     <span class="ml-5 ">Copyright © 2024 • Mimi Sofm.</span>
-     </ul>
+    <div class="flex justify-between gap-4 text-white border-t-2 border-[#9a9a9a] py-2 my-2">
+      <ul class="flex items-cente gap-6">
+        <li><a href="#" class="uppercase text-sm">about us</a></li>
+        <li><a href="#" class="uppercase text-sm">contact us</a></li>
+        <li><a href="#" class="uppercase text-sm">help</a></li>
+        <li><a href="#" class="uppercase text-sm">privacy policy</a></li>
+      </ul>
+        <span>Copyright © 2024 • Mimi Sofm.</span>
     </div>
   </footer>
 
