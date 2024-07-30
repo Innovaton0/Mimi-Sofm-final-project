8eb099712ea5b86aa4932ffa8f1782c37f917643
Inno Melkonjan <monkeytoffy@gmail.com>
2024-07-30 10:51:09 +0200

    all routes done branch complete

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,18 @@
+<template>
+
+<h1>About US</h1>
+
+<p>Sobre nosotros:nuestro perfil, cómo nos conocimos, nuestra ubicación, etc</p>
+
+</template>
+
+<script>
+
+  export default {
+    name: "AboutUs",
+    data() {
+      return {};
+    },
+  };
+
+</script>
---
+++ src/components/Help.vue
@@ -0,0 +1,20 @@
+<template>
+
+<h1>HELP</h1>
+
+<p> Aquí van las preguntas frecuentes, página de Ayuda</p>
+
+</template>
+
+<script>
+
+  export default {
+    name: "Help",
+    data() {
+      return {};
+    },
+  };
+
+</script>
+
+<style></style>
