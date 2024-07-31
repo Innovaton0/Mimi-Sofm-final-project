1c26bbd5325893210855eb6fea44b16efd8967d4
Inno Melkonjan <monkeytoffy@gmail.com>
2024-07-31 12:45:27 +0200

    input created with styles

--- src/components/TattooArtists.vue
+++ src/components/TattooArtists.vue
@@ -1,6 +1,22 @@
 <template>
-    <h1> TATTOO ARTIST </h1>
-    <button> Submit </button>
+    <main class="bg-[#F9EEDF] h-screen">
+        <div>
+            <div>NAVBAR</div>
+            <div class="ms-28 mt-48">
+             <label for="search" class="font-bold bg-black text-white text-center p-2">CATEGORÍAS
+             </label>
+             <input class="w-96 p-2" id="search" placeholder="Busca aquí" type="text">
+            </div>
+        </div>
+        <div class="mx-28">
+            <ul>
+                <li></li>
+            </ul>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </main>
 </template>
     
 <script>
