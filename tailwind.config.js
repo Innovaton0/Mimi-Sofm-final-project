8caa3db5531609722328f26e164d2f1d16b343c9
francamateus93 <francamateus@gmail.com>
2024-08-01 12:15:21 +0200

    Join-us finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ['"Montserrat"']
+    },
   },
+},
   variants: {
     extend: {},
   },
