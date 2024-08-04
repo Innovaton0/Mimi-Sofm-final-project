9892001f8229c06a7678f1f5cdd3ba50973070d1
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-04 20:24:41 +0200

    search bar component done

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,12 @@ import JoinUs from './components/JoinUs.vue';
 import Register from './components/Register.vue';
 import Help from './components/Help.vue';
 import AboutUs from "./components/AboutUs.vue"
+import Search from "./components/Search.vue";
+
 
 export default {
   name: "App",
-  components: { Home, TattooPage, LandingPage, TattooPage2, TattooArtists, ArtistProfile, JoinUs, Register, Help, AboutUs },
+  components: { Home, TattooPage, LandingPage, TattooPage2, TattooArtists, ArtistProfile, JoinUs, Register, Help, AboutUs, Search },
   data() {
     return {};
   },
