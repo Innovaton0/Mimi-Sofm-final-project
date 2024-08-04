0115e7d0d809396f3211a698de52c5e8445458de
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-04 14:31:38 +0200

    header part done with search img and grid positioning

--- src/components/TattooArtists.vue
+++ src/components/TattooArtists.vue
@@ -1,20 +1,26 @@
 <template>
-    <main class="bg-[#F9EEDF] h-screen">
-        <div>
-            <div>NAVBAR</div>
-            <div class="ms-28 mt-48">
-             <label for="search" class="font-bold bg-black text-white text-center p-2">CATEGORÍAS
-             </label>
-             <input class="w-96 p-2" id="search" placeholder="Busca aquí" type="text">
-            </div>
+    <header class="bg-[#F9EEDF] grid grid-cols-3 grid-rows-2 h-68">
+        <div class="col-start-1 col-end-4 row-start-1 row-end-1">
+            NAVBAR</div>
+
+        <div class="col-start-1 col-end-3 row-start-2 flex items-start justify-center">
+         <label for="search" class="font-bold bg-black text-white text-center py-2 px-10">CATEGORÍAS
+         </label>
+         <input class="w-[50rem] p-2" id="search" placeholder="Busca aquí" type="text">
         </div>
+        <div class="col-start-3 col-end-4 row-start-1 row-end-3 flex justify-end">
+            <img class="w-[30rem] h-[30rem] -translate-y-32" src="../assets/tattoo-tool2.png" alt="tattoo-tool">
+        </div>
+    </header>
+    <main class="bg-[#F9EEDF] h-screen">
+        
         <div class="mx-28">
             <ul>
-                <li></li>
+                <li>a</li>
             </ul>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
         </div>
     </main>
 </template>
