27e042c493277c6a2f71258c7fe96b8439ce308c
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-05 20:02:54 +0200

    created navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1> Navbar</h1>
+</template>
+    
+<script>
+    export default {
+        name: "Navbar",
+        data() {
+        return {
+
+        }
+    }
+    }
+</script>
