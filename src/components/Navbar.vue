d3833d4245bb5c75aec8e1bd3b7348c29261e9e7
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-05 23:46:57 +0200

    navbar responsive, routes linked missing que buscas double function

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,25 @@
 <template>
-    <h1> Navbar</h1>
+    <div class="flex justify-center items-center pt-8 z-10">
+        <div :class="bgColor" class="bg-black text-white align-bottom h-16 -me-4 flex-auto flex items-center ps-4 sm:ps-8">
+            <p>¿Qué buscas?</p>
+        </div>
+        <router-link to="/" :class="bgColor, circleColor" class="bg-black rounded-full w-32 h-32 z-20 flex justify-center items-center">
+            <img src="../assets/logo-nav.png" alt="logo">
+        </router-link>
+        <div :class="bgColor" class="bg-black text-white h-16 -ms-4 flex-auto flex justify-end items-center gap-5 pe-4 sm:pe-8">
+           <router-link to="/joinus">Únete</router-link>
+           <router-link to="/help">Ayuda</router-link>
+           <router-link to="/Us">Nosotros</router-link>
+        </div>
+    </div>
 </template>
     
 <script>
     export default {
         name: "Navbar",
+        props: { bgColor: {
+            type: String
+        }, circleColor: { type: String } },
         data() {
         return {
 
