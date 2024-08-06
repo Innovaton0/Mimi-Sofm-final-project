664bb715de18c0c6c666f0d463cac7b82bab45c2
francamateus93 <francamateus@gmail.com>
2024-08-06 10:09:38 +0200

    not found finished

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,9 +1,10 @@
 <template>
 
-    <h1>ERROR 404</h1>
-    <h3>Not Found</h3>
-    
-    
+  <div class="bg-[#F9EEDF] h-[100vh] font-mont text-black flex flex-col items-center justify-center">
+    <h1 class="text-6xl font-bold text-[#F09235] tracking-wider mb-4">ERROR 404</h1>
+    <h3 class="text-xl font-semibold ">Not Found</h3>
+  </div>
+
     </template>
     
     <script>
