5064d5da5408b3f5d9304d4079383f76f05cab85
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-06 08:34:29 +0200

    navbar complete, prop not working

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="flex justify-center items-center pt-8 z-10">
-        <div :class="bgColor" class="bg-black text-white align-bottom h-16 -me-4 flex-auto flex items-center ps-4 sm:ps-8">
-            <p>¿Qué buscas?</p>
-        </div>
-        <router-link to="/" :class="bgColor, circleColor" class="bg-black rounded-full w-32 h-32 z-20 flex justify-center items-center">
+        <button :class="bgColor" class="bg-black text-white align-bottom h-16 -me-4 flex-auto flex items-center ps-4 sm:ps-8">
+            <p @click="select = !select " class="after:content-['v'] after:font-bold after:ps-2">
+                ¿Qué buscas?
+                <div :class="bgColor" class="bg-black absolute p-5 flex flex-col gap-2" v-if="select">
+                    <router-link to="/tattoo">Tatuajes</router-link>
+                    <router-link to="/tattooartists">Tatuadores</router-link>
+                </div>
+            </p>
+        </button>
+        <router-link to="/" :class="bgColor, circleColor" class="bg-black rounded-full w-36 h-36 z-20 flex justify-center items-center">
             <img src="../assets/logo-nav.png" alt="logo">
         </router-link>
         <div :class="bgColor" class="bg-black text-white h-16 -ms-4 flex-auto flex justify-end items-center gap-5 pe-4 sm:pe-8">
@@ -19,10 +25,11 @@
         name: "Navbar",
         props: { bgColor: {
             type: String
-        }, circleColor: { type: String } },
+        }, 
+        circleColor: { type: String } },
         data() {
         return {
-
+          select: false
         }
     }
     }
