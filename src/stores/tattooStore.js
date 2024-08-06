5055977541d698932c907788d68fdcd7104d2f56
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-06 10:21:01 +0200

    installed pinia and its js file

---
+++ src/stores/tattooStore.js
@@ -0,0 +1,14 @@
+import {defineStore} from 'pinia'
+
+export const useTattooStore = defineStore('TattooStore', {
+    state: () => ({
+      artists: [],
+      images: []
+    }),
+    getters: {
+        
+    },
+    actions: {
+
+    }
+})
