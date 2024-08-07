872c8d9082fcb28ade6b2c7a1abe237b15394fd7
Marian Simó Díaz <mad@MADMacOS.local>
2024-08-07 12:35:36 +0200

    images reajusted and routerlink added to all of them

--- src/components/TattooPage.vue
+++ src/components/TattooPage.vue
@@ -1,62 +1,57 @@
 <template>
   <Navbar></Navbar>
-
-  <!-- Aquí va navbar -->
   <Search></Search>
 
-  <div
-    class="grid grid-cols-5 justify-between justify-items-center space-x-4 text-white my-20 mx-5"
-  >
+  <div class="grid grid-cols-5 gap-4 text-white my-20 mx-5">
     <!-- JAPONES -->
-    <div class="col-start-1 col-end-2 border border-black">
+    <RouterLink to="/tattoo2"><div>
       <img
-        src="../assets/TattooPage/Japones.jpg"
+        src="../assets/TattooPage/Japonés.jpg"
         alt="Torso con tatuaje de estilo japonés en brazos y pectoral"
       />
-      <RouterLink to="/tattoo2"
-        ><button class="bg-black w-full">JAPONES</button></RouterLink
-      >
-    </div>
+        <button class="bg-black w-full">JAPONES</button>
+    </div></RouterLink>
 
     <!-- FLORAL -->
-    <div class="overflow-hidden col-start-2 col-end-3 border border-red-700">
+    <RouterLink to="/tattoo2"><div>
       <img
         src="../assets/TattooPage/Floral.jpg"
         alt="Torso mujer con tatuajes florales en los costados"
       />
       <button class="bg-black w-full">FLORAL</button>
-    </div>
+    </div></RouterLink>
 
     <!-- ILUSTRACIONES -->
-    <div class="col-start-3 col-end-4">
+    <RouterLink to="/tattoo2"><div>
       <img
         src="../assets/TattooPage/Ilustraciones.jpg"
         alt="antebrazos con tatuajes coloridos ilustraciones"
       />
       <button class="bg-black w-full">ILUSTRACIONES</button>
-    </div>
+    </div></RouterLink>
 
     <!-- MINIMAL -->
-    <div class="col-start-4 col-end-5">
+    <RouterLink to="/tattoo2"><div>
       <img
         src="../assets/TattooPage/Minimal.jpg"
         alt="Tatuaje líneas minimalistas de sol con luna sobrepuesta en tobillo"
       />
       <button class="bg-black w-full">MINIMAL</button>
-    </div>
+    </div></RouterLink>
 
     <!-- PURE BLACK -->
-    <div class="col-start-5 col-end-6">
+    <RouterLink to="/tattoo2"><div>
       <img
-        src="../assets/TattooPage/Pure Black.jpg"
+        src="../assets/TattooPage/PureBlack.jpg"
         alt="Tatuaje color negro con patron floral sobre hombro, brazo y pectoral"
       />
       <button class="bg-black w-full">PURE BLACK</button>
-    </div>
+    </div></RouterLink>
   </div>
 
   <Footer></Footer>
 </template>
+
 
 <script>
 import Search from "./Search.vue";
