fda3e5b9130e79a59c8f06eaa5a36d5eeb3de792
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-07 22:08:51 +0200

    about us responsive

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,19 +1,19 @@
 <template>
 
-<section class="bg-[#F9EEDF] h-full font-mont"> 
   <Navbar></Navbar>
+  <section class="bg-[#F9EEDF] h-full font-mont"> 
 
   <!-- NOSOTROS -->
   <p class="font-bold border-b-8 border-[#F09235]/100 decoration-[6px] text-2xl w-[20rem] text-end">NOSOTROS</p>
 
-  <div class="flex justify-center mt-10 items-center space-x-20">
+  <div class="flex justify-center mt-10 items-center gap-6">
   
     <!-- MATEUS -->
-   <div class="flex flex-col gap-5 items-center">
+   <div class="flex flex-col  items-center">
    <img
       src="../assets/Nosotros/T06SBAM50LR-U070HGSUD0C-90cde90c6623-512.png"
       alt="Mateus"
-      class="w-32 h-32 rounded-full object-cover"
+      class="w-14 h-14 sm:w-32 sm:h-32 rounded-full object-cover"
     /> 
     
     
@@ -21,32 +21,32 @@
   </div>
 
 <!-- MARIANTO -->
-  <div class="flex flex-col gap-5 items-center">
+  <div class="flex flex-col items-center">
    <img
       src="../assets/Nosotros/T06SBAM50LR-U071B5MU0L8-f736a6aa7cee-512.jpg"
       alt="Marianto"
-      class="w-32 h-32 rounded-full object-cover"
+      class="w-14 h-14 sm:w-32 sm:h-32 rounded-full object-cover"
     /> 
     <p class="font-bold">MARIANTO</p>
   </div>
 
 <!-- INNO -->
-  <div class="flex flex-col gap-5 items-center">
+  <div class="flex flex-col  items-center">
     <img
       src="../assets/Nosotros/innp.png"
       alt="Inno"
-      class="w-32 h-32 rounded-full object-cover"
+      class="w-14 h-14 sm:w-32 sm:h-32 rounded-full object-cover"
     />
     
      <p class="font-bold">INNO</p>
   </div>
 
 <!-- NACHA -->
-  <div class="flex flex-col gap-5 items-center">
+  <div class="flex flex-col  items-center">
     <img
       src="../assets/Nosotros/IMG_7723.png"
       alt="Nacha"
-      class="w-32 h-32 rounded-full object-cover"
+      class="w-14 h-14 sm:w-32 sm:h-32 rounded-full object-cover"
     />
     
     
@@ -70,24 +70,24 @@
 
             <!-- Campo de nombre -->
             <div class="mb-4">
-                <label for="name"</label>
+                <label for="name"></label>
                 <input type="text" id="name" name="name" placeholder="Nombre"
-                    class="px-4 w-[500px] py-2 mt-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:border-transparent" />
+                    class="px-4 w-[27rem] lg:w-[35rem] py-2 mt-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:border-transparent" />
             </div>
 
             <!-- Campo de email -->
             <div class="mb-4">
-                <label for="email"</label>
+                <label for="email"></label>
                 <input type="email" id="email" name="email" placeholder="tucorreo@ejemplo.com"
-                    class="px-4 w-[500px] py-2 mt-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:border-transparent" />
+                    class="px-4 w-[27rem] lg:w-[35rem] py-2 mt-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:border-transparent" />
             </div>
 
 
             <!-- Área de texto -->
             <div class="mb-4">
-                <label for="message"</label>
+                <label for="message"></label>
                 <textarea id="about" name="about" rows="4" placeholder="Mensaje"
-                    class="px-4 w-[500px] py-2 mt-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:border-transparent"></textarea>
+                    class="px-4 w-[27rem] lg:w-[35rem] py-2 mt-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:border-transparent"></textarea>
             </div>
 
             <!-- Checkbox -->
@@ -101,7 +101,7 @@
             <!-- Botón de envío -->
             <div class="mt-6">
                 <button type="submit"
-                    class=" bg-[#F09235] text-white font-bold py-2 px-4 w-[500px] rounded-lg text-sm hover:bg-[#8A312E] focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:ring-offset-2 focus:ring-offset-white">
+                    class=" bg-[#F09235] text-white font-bold py-2 px-4 w-[27rem] lg:w-[35rem] rounded-lg text-sm hover:bg-[#8A312E] focus:outline-none focus:ring-2 focus:ring-[#F09235] focus:ring-offset-2 focus:ring-offset-white">
                     Enviar
                 </button>
             </div>
