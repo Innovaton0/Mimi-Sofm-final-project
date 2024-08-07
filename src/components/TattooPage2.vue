54f718168becf8f7c857171ba8b9ba9010146bee
francamateus93 <francamateus@gmail.com>
2024-08-07 13:01:33 +0200

    tattopage2 almost finished

--- src/components/TattooPage2.vue
+++ src/components/TattooPage2.vue
@@ -1,14 +1,35 @@
 <template>
-    <Navbar></Navbar>
-    <Search></Search>
-    <Footer></Footer>
+  <Navbar></Navbar>
+  <Search></Search>
 
-    <div v-if="images">
-    <div v-for="(image, index) in images" :key="index">
-      <img :src="image.src.medium" />
+  <!-- Buttons -->
+  <div class="grid grid-cols-5 py-8 justify-items-center"> 
+    <div>
+      <button class="bg-black text-white w-48 p-1 hover:bg-[#651c19] active:bg-[#F09235]">JAPONES</button>
     </div>
-  </div> 
+    <div>
+      <button class="bg-black text-white w-48 p-1 hover:bg-[#651c19] active:bg-[#F09235]">FLORAL</button>
+    </div>
+    <div>
+      <button class="bg-black text-white w-48 p-1 hover:bg-[#651c19] active:bg-[#F09235]">ILUSTRACIONES</button>
+    </div>
+    <div>
+      <button class="bg-black text-white w-48 p-1 hover:bg-[#651c19] active:bg-[#F09235]">MINIMAL</button>
+    </div>
+    <div>
+      <button class="bg-black text-white w-48 p-1 hover:bg-[#651c19] active:bg-[#F09235]">PURE BLACK</button>
+    </div>
+  </div>
 
+
+  <!-- Grid imgs -->
+    <div v-if="images" class="flex flex-wrap gap-3 mx-6 pb-8">
+      <div v-for="(image, index) in images" :key="index" class="flex-[2_2_200px]">
+        <img :src="image.src.medium" class="w-[100%] h-[150px] object-cover"/>
+      </div>
+    </div> 
+    
+  <Footer></Footer>
 </template>
     
 <script>
