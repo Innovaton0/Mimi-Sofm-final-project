10d8241020b3739568ad5fad078d9269c84c91f1
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-08 07:57:07 +0200

    tattoo page resonsive and added bg-tool image

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <label for="search" class="font-bold bg-black text-white text-center py-2 px-5 sm:px-10">CATEGORÍAS</label>
-    <input class="w-[50rem] p-2" id="search" placeholder="Busca aquí" type="text">
+    <input class="w-72 sm:w-[20rem] md:w-[30rem] lg:w-[50rem] p-2 z-20" id="search" placeholder="Busca aquí" type="text">
   </template>
   
   <script>
