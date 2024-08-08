953dd69919fbaed3d6f098a0f7bf8250d6a54b8c
NachadeBosquetro <ignaciaossandon@gmail.com>
2024-08-08 10:27:26 +0200

    logo goes to home

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@
                 </div>
             </button>
         </div>
-        <router-link to="/" :style="circleColor" class="bg-black rounded-full w-36 h-36 z-20 flex justify-center items-center">
+        <router-link to="/home" :style="circleColor" class="bg-black rounded-full w-36 h-36 z-20 flex justify-center items-center">
             <img src="../assets/logo-nav.png" alt="logo">
         </router-link>
         <div :style="bgColor" class="bg-black text-white h-16 -ms-4 flex-auto flex justify-end items-center gap-5 pe-4 sm:pe-8">
